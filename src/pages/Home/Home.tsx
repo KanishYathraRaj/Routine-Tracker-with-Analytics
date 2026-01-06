2f99e3fee673d71e77f3bc6b5c2c3177100da7e3
KanishYathraRaj <Kanish.aims@gmail.com>
2026-01-07 01:23:17 +0530

    Todo list component is added

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import TodoList from '../../components/TodoList';
+
+const Home = () => {
+    return (
+        <div className="min-h-screen flex items-start bg-gray-100 p-4">
+            <TodoList />
+        </div>
+    );
+};
+
+export default Home;
